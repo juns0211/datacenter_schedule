a1f4437c347e60d09d4bdda9c9ad4ab6e4f94f2a
juns_chen <mailkill2000@gmail.com>
2022-11-17 17:17:26 +0800

    0.0.13 更新讀取yaml檔工具

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='datacenter_schedule',
-    version='0.0.12',
+    version='0.0.13',
     description='數據中心系統專用排程器工具',
     py_modules=['clean', 'params', 'storage'],
-    packages=find_packages()
+    packages=find_packages(),
+    package_data={'utils':['config.yaml']}
 )
--- utils/setting.py
+++ utils/setting.py
@@ -1,13 +1,20 @@
 from pathlib import Path
 import yaml
 from decouple import config
+from importlib import resources
 BASE_PATH = Path(__file__).parent
 
 # 讀取設定檔
-with (BASE_PATH / 'config.yaml').open(encoding='utf8') as f:
+with resources.open_binary('utils', 'config.yaml') as f:
     cf = yaml.load(f, Loader=yaml.Loader)
     MSSQL_DB_SETTING = cf['MSSQL_DB_SETTING']
     TEST_MSSQL_DB_SETTING = cf['TEST_MSSQL_DB_SETTING']
+
+# 讀取設定檔
+# with (BASE_PATH / 'config.yaml').open(encoding='utf8') as f:
+#     cf = yaml.load(f, Loader=yaml.Loader)
+#     MSSQL_DB_SETTING = cf['MSSQL_DB_SETTING']
+#     TEST_MSSQL_DB_SETTING = cf['TEST_MSSQL_DB_SETTING']
 
 # MSSQL
 from .db import Database
