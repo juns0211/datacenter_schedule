00c6a08356fa4c4ad1c048a242e773a6239e8fad
william_liu <0336_william@rfr.com.tw>
2022-11-23 17:18:46 +0800

    統一使用pymssql

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datacenter_schedule',
-    version='0.0.15',
+    version='0.0.16',
     description='數據中心系統專用排程器工具',
     py_modules=['clean', 'params', 'storage'],
     packages=find_packages(),
